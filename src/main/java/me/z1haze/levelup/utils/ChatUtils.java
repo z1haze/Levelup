6c97e4b6e0939ce80c157d6d42c8c6ddd52ea2d3
BuildTools <unconfigured@null.spigotmc.org>
2023-08-24 11:18:21 -0700

    add hex color code support to messages

--- src/main/java/me/z1haze/levelup/utils/ChatUtils.java
+++ src/main/java/me/z1haze/levelup/utils/ChatUtils.java
@@ -4,8 +4,30 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 
 public class ChatUtils {
-    public static String colorize(String str) {
-        return ChatColor.translateAlternateColorCodes('&', str);
+
+    private static final String WITH_DELIMITER = "((?<=%1$s)|(?=%1$s))";
+
+    public static String colorize(String text) {
+        String[] texts = text.split(String.format(WITH_DELIMITER, "&"));
+
+        StringBuilder finalText = new StringBuilder();
+
+        for (int i = 0; i < texts.length; i++){
+            if (texts[i].equalsIgnoreCase("&")){
+                //get the next string
+                i++;
+                if (texts[i].charAt(0) == '#'){
+                    finalText.append(net.md_5.bungee.api.ChatColor.of(texts[i].substring(0, 7))).append(texts[i].substring(7));
+                }else{
+                    finalText.append(ChatColor.translateAlternateColorCodes('&', "&" + texts[i]));
+                }
+            }else{
+                finalText.append(texts[i]);
+            }
+        }
+
+        return finalText.toString();
+
     }
 
     public static void sendColorized(CommandSender sender, String str) {
@@ -13,5 +35,4 @@ public class ChatUtils {
             sender.sendMessage(colorize(str));
         }
     }
-
 }
